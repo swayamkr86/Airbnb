48b2d270b06cba1208d8b4150a2c9cc1a2ba8323
swayamkr86 <137387677+swayamkr86@users.noreply.github.com>
2025-05-19 21:23:45 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,8 +14,8 @@ export const sighUp=async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT = "production",
-            sameSite: "strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
@@ -41,8 +41,8 @@ export const login = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT = "production",
-            sameSite: "strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
