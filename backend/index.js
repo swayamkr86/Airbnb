2f66ea34d32064bc8a11348e350375820e36cee3
swayamkr86 <137387677+swayamkr86@users.noreply.github.com>
2025-05-19 22:34:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://airbnb-x5ai.onrender.com",
     credentials:true
 }))
 
